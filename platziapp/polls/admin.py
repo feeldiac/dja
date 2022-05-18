12ceb64491395f455083ec5be8a337bc695e0ea3
Fernando Díaz <feeldiac@gmail.com>
2022-05-18 14:05:39 -0500

    Changed field order in admin create

--- platziapp/polls/admin.py
+++ platziapp/polls/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Choice, Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
     inlines = [
         ChoiceInline,
     ]
